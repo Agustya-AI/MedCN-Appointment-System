f63ab935e7e65e240cbb8fa5fa01dcfa4f2470bf
imdigitalashish <imdigitalashish@gmail.com>
2025-07-21 11:56:55 +0530

    Deployment Requirements File

--- backend/api/example_router/routes.py
+++ backend/api/example_router/routes.py
@@ -3,6 +3,9 @@ from asgiref.sync import sync_to_async
 from api.config import configureDjangoSettings
 from starlette.concurrency import run_in_threadpool
 
+from fastapi import Request
+
+
 configureDjangoSettings()
 
 from example_app.models import ExampleModel
@@ -18,3 +21,17 @@ async def getData():
     all_data = await run_in_threadpool(lambda: list(ExampleModel.objects.all()))
     print(all_data)
     return {"message": "DETA"}
+
+
+@router.post("/admin")
+async def getadminLogin(request: Request):
+    
+    data = await request.json()
+    
+    if data["username"] == "admin" and data["password"] == "admin":
+        return {"message": "Login successful"}
+    else:
+        return {"message": "Login failed"}
+    
+    
+    
