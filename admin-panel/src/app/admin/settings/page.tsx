345108e1ab3568dcfbac19d208453b327d8cbec9
imdigitalashish <imdigitalashish@gmail.com>
2025-07-29 15:46:45 +0530

    Added Settings Page

---
+++ admin-panel/src/app/admin/settings/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AdminPortalLayout from '../_components/AdminPortalLayout'
+
+export default function page() {
+  return (
+    <AdminPortalLayout>
+        <p></p>
+    </AdminPortalLayout>
+  )
+}
