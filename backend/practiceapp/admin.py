c2264274fc3acd29807b017349094258cddcec29
imdigitalashish <imdigitalashish@gmail.com>
2025-08-09 18:58:52 +0530

    Appointment System

--- backend/practiceapp/admin.py
+++ backend/practiceapp/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import PractionerUser, PracticeRegistry, PractionerRegistry, AppointmentType, AvailabilitySlot
+from .models import PractionerUser, PracticeRegistry, PractionerRegistry, AppointmentType, AvailabilitySlot, PracticeMembers
 
 admin.site.register(PractionerUser)
 admin.site.register(PractionerRegistry)
 admin.site.register(PracticeRegistry)
 admin.site.register(AppointmentType)
 admin.site.register(AvailabilitySlot)
+admin.site.register(PracticeMembers)
